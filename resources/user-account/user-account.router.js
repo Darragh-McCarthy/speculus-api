0624d3980d4c8e56ac3905f49717c946d12cde34
Darragh McCarthy <darraghjames@gmail.com>
2019-09-06 14:43:40 +0100

    update

--- resources/user-account/user-account.router.js
+++ resources/user-account/user-account.router.js
@@ -12,6 +12,10 @@ router.get("/", csrfProtection, async (req, res) => {
   const token = req.csrfToken();
   res.cookie("XSRF-TOKEN", token);
   const user = await UserModel.findById(res.locals.userId);
+  if (!user) {
+    res.status(401).end();
+    return;
+  }
   const ratings = await getAllRatings(res.locals.userId);
   const notifications = await getNotifications({ userId: res.locals.userId });
   res.json({ data: { user, ratings, notifications } });
