8e2fc9e2aea70d96bda2b3078ecb7944008971d8
Darragh McCarthy <darraghjames@gmail.com>
2019-09-28 23:03:24 +0100

    update

--- resources/user-account/user-account.router.js
+++ resources/user-account/user-account.router.js
@@ -9,7 +9,6 @@ router.get("/", async (req, res) => {
     res.status(401).end();
     return;
   }
-  console.log(user);
   const notifications = await getNotifications({ userId: res.locals.user.id });
   res.json({
     data: { user: user.clientSideObject, notifications }
